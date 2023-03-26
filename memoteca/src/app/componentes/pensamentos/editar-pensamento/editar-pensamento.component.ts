db487c8284b35cad89ae477cd0ba80fb831e7c14
Hélio Márcio Filho <upmost-octane03@icloud.com>
2023-03-26 14:13:41 +0100

    Refactored with Reactive Forms

--- memoteca/src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Pensamento} from "../pensamento";
 import {PensamentoService} from "../pensamento.service";
 import {ActivatedRoute, Router} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-editar-pensamento',
@@ -10,24 +11,55 @@ import {ActivatedRoute, Router} from "@angular/router";
 })
 export class EditarPensamentoComponent implements OnInit {
 
-  pensamento!: Pensamento;
+  formulario!: FormGroup;
 
   constructor(
     private service: PensamentoService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      id: [null, [Validators.required]],
+      conteudo: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/(.|\s)*\S(.|\s)*/)
+      ])],
+      autoria: ['', Validators.compose([
+        Validators.required,
+        Validators.minLength(3)
+      ])],
+      modelo: ['modelo1', [Validators.required]]
+    });
+
     const id = this.route.snapshot.paramMap.get('id');
-    this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => this.pensamento = pensamento);
+
+    this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => {
+      this.formulario.get('id')?.setValue(id);
+      this.formulario.get('conteudo')?.setValue(pensamento.conteudo);
+      this.formulario.get('autoria')?.setValue(pensamento.autoria);
+      this.formulario.get('modelo')?.setValue(pensamento.modelo);
+    });
   }
 
   editarPensamento() {
-    this.service.editar(this.pensamento).subscribe(() => this.router.navigate(['listarPensamento']));
+    let pensamento: Pensamento = {
+      id: this.formulario.get('id')?.value,
+      conteudo: this.formulario.get('conteudo')?.value,
+      autoria: this.formulario.get('autoria')?.value,
+      modelo: this.formulario.get('modelo')?.value
+    };
+
+    this.service.editar(pensamento).subscribe(() => this.router.navigate(['listarPensamento']));
   }
 
   cancelar() {
     this.router.navigate(['listarPensamento']);
+  }
+
+  habilitarBotao(): string {
+    return this.formulario.valid ? 'botao' : 'botao__desabilitado';
   }
 }
